82e8de0d741a4853e4d9a842744a6f3fab682ed1
danizagutierrez <145223570+danizagutierrez@users.noreply.github.com>
2023-11-04 13:27:48 -0700

    Admin controller, restau model and restau repository

---
+++ src/main/java/com/example/eatmania/Models/RestaurantRepository.java
@@ -0,0 +1,12 @@
+package com.example.eatmania.Models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RestaurantRepository extends JpaRepository<RestaurantModel, Long> {
+
+    List<RestaurantModel> findRestaurantModelByNameContainingIgnoreCase (String name);
+    List<RestaurantModel> findRestaurantModelByCuisineTypeIgnoreCase(String cuisineType);
+
+}
---
+++ src/main/java/com/example/eatmania/controller/AdminController.java
@@ -0,0 +1,71 @@
+package com.example.eatmania.controller;
+
+import com.example.eatmania.Models.RestaurantModel;
+import com.example.eatmania.Models.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+        @Autowired
+        RestaurantRepository restaurantRepo;
+
+        // Creates a new restaurant
+        @PostMapping(path = "/restaurant")
+        public ResponseEntity<RestaurantModel> createRestaurant(@RequestBody RestaurantModel restaurant){
+                try{
+                        RestaurantModel newRestaurant = new RestaurantModel(restaurant.getName(), restaurant.getPhoneNumber(), restaurant.getCuisineType(), restaurant.getRating(),
+                                restaurant.getDescription(), restaurant.getWebsite());
+                        restaurantRepo.save(newRestaurant);
+                        return new ResponseEntity<>(newRestaurant, HttpStatus.CREATED);
+                }catch (Exception e){
+                        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+        }
+
+        //Deletes a restaurant
+
+        //Retrieves restaurant list with info by using search string (optional)
+
+        @GetMapping(path = "/restaurantlist")
+        public ResponseEntity<List<RestaurantModel>> getRestaurants(@RequestParam(required = false) String searchString){
+
+                try{
+                        List<RestaurantModel> restaurants = new ArrayList<RestaurantModel>();
+
+                        if(searchString == null){
+                                restaurantRepo.findAll().forEach(restaurants::add);
+                        }
+                        else {
+                                restaurantRepo.findRestaurantModelByNameContainingIgnoreCase(searchString).forEach(restaurants::add);
+                                restaurantRepo.findRestaurantModelByCuisineTypeIgnoreCase(searchString).forEach(restaurants::add);
+                        }
+                        if(restaurants.isEmpty()){
+                                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                        }
+                        return new ResponseEntity<>(restaurants, HttpStatus.OK);
+                }
+                catch (Exception e){
+                        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+        }
+
+        //Creates a food item
+
+        //Deletes a food item
+
+
+
+
+
+}
