b3f4b4034c616663b24b5c5ba05c5b00cb43d7db
evelyntsyang <141536128+evelyntsyang@users.noreply.github.com>
2023-11-19 15:21:01 -0800

    Change return login to user mode and add admin user to user table

--- src/main/java/com/example/eatmania/controller/MainController.java
+++ src/main/java/com/example/eatmania/controller/MainController.java
@@ -7,8 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 
 @CrossOrigin(origins = "*")
@@ -30,7 +32,7 @@ public class MainController {
     }
 
     @GetMapping("/login/{email}/{password}")
-    public String LoginExample (@PathVariable String email, @PathVariable String password ){
+    public ResponseEntity<UserModel> LoginExample (@PathVariable String email, @PathVariable String password ){
 
 
        if( email != null && password != null ){
@@ -39,16 +41,16 @@ public class MainController {
 
            for(int i = 0 ; i < userList.size(); i++){
                if( userList.get(i).getUserEmail().equals(email)  &&  userList.get(i).getPassword().equals(password)){
-                   return "You successfully login";
+                   return ResponseEntity.ok( userList.get(i) );
                }
            }
 
-           return "Your email or password is incorrect";
+           return ResponseEntity.badRequest().build(); // Use build() to create a ResponseEntity
        }
-       //Change this part later.
-        return "Your email or password is missingS";
-    }
 
+        // Change this part later.
+        return ResponseEntity.badRequest().build(); // Use build() to create a ResponseEntity
+    }
 
 
  @Autowired
@@ -218,7 +220,51 @@ public class MainController {
         }
 
         return ResponseEntity.badRequest().body("Please enter a valid food price");
-        //search by reivew
+
     }
 
+
+
+    //search by reivew
+    @GetMapping("/GetAllReviews")
+    public List<ReviewModel> GetReview(){
+        //foodRepo - database name
+        List<ReviewModel> reviews = reviewRepo.findAll();
+        return reviews;
+    }
+
+
+    //searchbyreviewratingandfooditem
+    //http://localhost:8080/api/searchByPrice?maxPrice=10
+    @GetMapping("/searchByReview")
+    public ResponseEntity<?> searchByReview(@RequestParam double maxReview, @RequestParam Long foodId) {
+
+        if (maxReview >= 0) {
+
+            List<ReviewModel> review_List = reviewRepo.findAll();
+
+            if (!review_List.isEmpty()) {
+                // Sort reviews by rating in descending order
+                review_List.sort(Comparator.comparingDouble(ReviewModel::getRating).reversed());
+
+                // Filter reviews based on the maximum rating
+                List<ReviewModel> matchingReviews = review_List.stream()
+                        .filter(review -> review.getRating() <= maxReview)
+                        .collect(Collectors.toList());
+
+                if (!matchingReviews.isEmpty()) {
+                    return ResponseEntity.ok(matchingReviews);
+                } else {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                            .body("No review found within the specified rating range for the given food ID.");
+                }
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("No reviews available for the given food ID.");
+            }
+        }
+        return ResponseEntity.badRequest().body("Please enter a valid review rating.");
+    }
 }
+
+
